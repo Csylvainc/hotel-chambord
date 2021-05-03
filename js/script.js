95d13728be49114918123b42f94bfd3c1504869a
Csylvainc <campos.sylvain@gmail.com>
2021-05-03 16:17:37 +0200

    nouvelle version

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(function(){
-// ---------------menu simple en mode tablette et smartphone---------------//
+// ---------------Modification des liens du menu en tablette et smartphone---------------//
 $("#menuChambre").on('click',function(){
     if (window.matchMedia("(max-width: 1200px)").matches)
     $(this).attr('href',"/chambres.html");
@@ -101,12 +101,140 @@ $("#menuHotel").on('click',function(){
         $("#menuRestaurant").toggleClass('cachem');
         $("#menuSpa").toggleClass('cachem');
         $("#menuHotel").toggleClass('cachem');
+        $("#jeffrey").toggleClass('cachem');
+        $("#jarvis").toggleClass('cachem');
         $("#menuRestaurantElt").addClass('cache');
         $("#menuChambreElt").addClass('cache');
         $("#menuSpaElt").addClass('cache');
         $("#menuHotelElt").addClass('cache');
         
-    })
+    });
 
+
+
+// -----------------------Changement de langue---------//
+$('#fr').on('click',function(){
+    alert('le site va être traduit en Francais')
+});
+$('#en').on('click',function(){
+    alert('le site va être traduit en Anglais')
+});
+
+
+// ---------------------Reservation---------------------------//
+
+
+// date picker------------//
+$.datepicker.regional['fr'] = {
+    closeText: 'Fermer',
+    prevText: '&#x3c;Préc',
+    nextText: 'Suiv&#x3e;',
+    currentText: 'Aujourd\'hui',
+    monthNames: ['Janvier','Fevrier','Mars','Avril','Mai','Juin',
+    'Juillet','Aout','Septembre','Octobre','Novembre','Decembre'],
+    monthNamesShort: ['Jan','Fev','Mar','Avr','Mai','Jun',
+    'Jul','Aou','Sep','Oct','Nov','Dec'],
+    dayNames: ['Dimanche','Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi'],
+    dayNamesShort: ['Dim','Lun','Mar','Mer','Jeu','Ven','Sam'],
+    dayNamesMin: ['Di','Lu','Ma','Me','Je','Ve','Sa'],
+    weekHeader: 'Sm',
+    dateFormat: 'dd-mm-yy',
+    firstDay: 1,
+    isRTL: false,
+    showMonthAfterYear: false,
+    yearSuffix: '',
+    minDate: 0,
+    maxDate: '+12M +0D',
+    numberOfMonths: 2,
+    showButtonPanel: true
+    };
+    $( "#datepicker" ).datepicker( "option",
+    $.datepicker.regional['fr'] );
+//   $.datepicker.setDefaults($.datepicker.regional['fr']);
+
+var dateFormat = "mm/dd/yy",
+from = $( "#from" )
+  .datepicker({
+    defaultDate: "+1w",
+    changeMonth: true,
+    numberOfMonths: 3,
+    dayNamesShort: $.datepicker.regional[ "fr" ].dayNamesShort,
+    dayNames: $.datepicker.regional[ "fr" ].dayNames,
+    monthNamesShort: $.datepicker.regional[ "fr" ].monthNamesShort,
+    dayNamesMin: $.datepicker.regional[ "fr" ].dayNamesMin,
+    monthNames: $.datepicker.regional[ "fr" ].monthNames
+  })
+  .on( "change", function() {
+    to.datepicker( "option", "minDate", getDate( this ) );
+  }),
+to = $( "#to" ).datepicker({
+  defaultDate: "+1w",
+  changeMonth: true,
+  numberOfMonths: 3,
+  dayNamesShort: $.datepicker.regional[ "fr" ].dayNamesShort,
+  dayNames: $.datepicker.regional[ "fr" ].dayNames,
+  monthNamesShort: $.datepicker.regional[ "fr" ].monthNamesShort,
+  dayNamesMin: $.datepicker.regional[ "fr" ].dayNamesMin,
+  monthNames: $.datepicker.regional[ "fr" ].monthNames
+})
+.on( "change", function() {
+  from.datepicker( "option", "maxDate", getDate( this ) );
+});
+
+function getDate( element ) {
+var date;
+try {
+  date = $.datepicker.parseDate( dateFormat, element.value );
+} catch( error ) {
+  date = null;
+}
+
+return date;
+}
+// $('#datedebut').datepicker( $.datepicker.regional[ "fr" ] );
+// --------Test Date fin apres date debut----------------//
+$('#datefin').on('change',function(){
+    let endDate = $(this).datepicker("getDate");
+    let startDate = $('#datedebut').datepicker("getDate");
+    let diff = endDate - startDate;
+    let diffjours = diff / 86400000;
+    console.log(diffjours);
+        if(diffjours < 0){
+        alert("vous ne pouvez pas partir avant d'ariver");
+    }
+})
+
+
+// ------------- Video----------------------------------//
+$("#vidLinks a").click(function () {
+    $("#modal").fadeIn();
+  });
+  $("#modal span").click(function () {
+    $("#modal").fadeOut();
+    $("#mainVid").attr("src", "");
+  });
+
+  
+
+// -----------------fin Video---------------------------------------//
+
+// ----------------------Début Chambre------------------------------//
+$(".visible").on('click',function(){
+    $("#chambreClassique").toggleClass('cache');
+});
+$(".visible1").on('click',function(){
+    $("#chambreConfort").toggleClass('cache');
+});
+$(".visible2").on('click',function(){
+    $("#chambreLuxe").toggleClass('cache');
+});
+$(".visible3").on('click',function(){
+    $("#chambreSuite").toggleClass('cache');
+});
+// ----------------------Fin Chambre------------------------------//
+
+
+
+ 
 
 });/////////////////////////////////////////////////////////////////////////////////////////////////////
